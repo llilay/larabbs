6e495b4e1ab7c8b9f1efdda9aac34fd5a9f5edb1
llilay <2316672811@qq.com>
2018-09-14 08:09:30 +0000

    User can upload topic image

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -73,6 +73,14 @@
         $(document).ready(function(){
             var editor = new Simditor({
                 textarea: $('#editor'),
+                upload: {
+                    url: '{{ route('topics.upload_image') }}',//处理上传图片的 URL；
+                    params: {_token: '{{ csrf_token() }}'},//表单提交的参数，Laravel 的 POST 请求必须带防止 CSRF 跨站请求伪造的 _token 参数；
+                    fileKey: 'upload_file',//是服务器端获取图片的键值，我们设置为 upload_file;
+                    connectionCount: 3,//最多只能同时上传 3 张图片；
+                    leaveConfirm: '文件上传中，关闭此页面将取消上传。'//上传过程中，用户关闭页面时的提醒。
+                },
+                pasteImage: true, //设定是否支持图片黏贴上传
             });
         });
     </script>
--- routes/web.php
+++ routes/web.php
@@ -32,5 +32,6 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
