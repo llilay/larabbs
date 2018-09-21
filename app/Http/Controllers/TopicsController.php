a26027a6ee3e0e19a6cf88212fbcab44189aaa17
llilay <2316672811@qq.com>
2018-09-21 07:30:03 +0000

    Sidebar active users

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use App\Models\Category;
 use App\Handlers\ImageUploadHandler;
+use App\Models\User;
 use Auth;
 
 class TopicsController extends Controller
@@ -19,10 +20,11 @@ class TopicsController extends Controller
         ]);
     }
 
-	public function index(Request $request, Topic $topic)
+	public function index(Request $request, Topic $topic, User $user)
 	{
-		$topics = $topic->withOrder($request->order)->paginate(20);
-		return view('topics.index', compact('topics'));
+        $topics = $topic->withOrder($request->order)->paginate(20);
+        $active_users = $user->getActiveUsers();
+        return view('topics.index', compact('topics', 'active_users'));
 	}
 
     public function show(Request $request, Topic $topic)
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,8 +9,8 @@ use Auth;
 
 class User extends Authenticatable
 {
+    use Traits\ActiveUserHelper;
     use HasRoles;
-
     use Notifiable {
         notify as protected laravelNotify;
     }
