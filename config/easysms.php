a53a444ac65edd3a50782284fbd4c900a5140e28
llilay <2316672811@qq.com>
2018-09-25 08:25:38 +0000

    add easysms

---
+++ config/easysms.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    // HTTP 请求的超时时间（秒）
+    'timeout' => 5.0,
+
+    // 默认发送配置
+    'default' => [
+        // 网关调用策略，默认：顺序调用
+        'strategy' => \Overtrue\EasySms\Strategies\OrderStrategy::class,
+
+        // 默认可用的发送网关
+        'gateways' => [
+            'juhe',
+        ],
+    ],
+    // 可用的网关配置
+    'gateways' => [
+        'errorlog' => [
+            'file' => '/tmp/easy-sms.log',
+        ],
+        'juhe' => [
+            'app_key' => env('JUHE_APP_KEY'),
+        ],
+    ],
+];
