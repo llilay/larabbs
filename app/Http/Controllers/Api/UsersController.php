6f51294a67da22d51315995e747a0e97a3d900d6
llilay <2316672811@qq.com>
2018-09-25 10:05:13 +0000

    user can register using phone

---
+++ app/Http/Controllers/Api/UsersController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\Api\UserRequest;
+
+class UsersController extends Controller
+{
+    public function store(UserRequest $request)
+    {
+        $verifyData = \Cache::get($request->verification_key);
+
+        if (!$verifyData) {
+            return $this->response->error('验证码已失效', 422);
+        }
+
+        //比对验证码是否与缓存中一致, hash_equals 是可防止时序攻击的字符串比较
+        if (!hash_equals($verifyData['code'], $request->verification_code)) {
+            // 返回401
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'phone' => $verifyData['phone'],
+            'password' => bcrypt($request->password),
+        ]);
+
+        // 清除验证码缓存
+        \Cache::forget($request->verification_key);
+
+        return $this->response->created();
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -33,7 +33,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'introduction', 'avatar'
+        'name', 'email', 'password', 'introduction', 'avatar', 'phone'
     ];
 
     /**
