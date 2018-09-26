07806e6e3ead299832f66e096c991fa2a8178b43
llilay <2316672811@qq.com>
2018-09-26 08:39:43 +0000

    social authorizations

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,7 +33,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'introduction', 'avatar', 'phone'
+        'name', 'phone', 'email', 'password', 'introduction', 'avatar',
+        'weixin_openid', 'weixin_unionid'
     ];
 
     /**
