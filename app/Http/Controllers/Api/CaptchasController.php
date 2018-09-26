0bf899294ff0676323fc56dbb77f0e70576e9072
llilay <2316672811@qq.com>
2018-09-26 06:56:35 +0000

    add captcha

---
+++ app/Http/Controllers/Api/CaptchasController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Gregwar\Captcha\CaptchaBuilder;
+use App\Http\Requests\Api\CaptchaRequest;
+
+class CaptchasController extends Controller
+{
+    public function store(CaptchaRequest $request, CaptchaBuilder $captchaBuilder)
+    {
+        $key = 'captcha-' . str_random(15);
+        $phone = $request->phone;
+
+        $captcha = $captchaBuilder->build();
+        $expiredAt = now()->addMinutes(2);
+        \Cache::put($key, [
+            'phone' => $phone,
+            'code' => $captcha->getPhrase() //使用 getPhrase 方法获取验证码文本
+        ], $expiredAt);
+
+        //返回 captcha_key，过期时间以及 inline 方法获取的 base64 图片验证码
+        $result = [
+            'captcha_key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(),
+            'captcha_image_content' => $captcha->inline()
+        ];
+
+        return $this->response->array($result)->setStatusCode(201);
+    }
+}
