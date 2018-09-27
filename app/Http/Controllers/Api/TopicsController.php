020915ea542890ac80e57de656439f3bf5c790b5
llilay <2316672811@qq.com>
2018-09-27 09:00:35 +0000

    topic delete api

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -40,4 +40,12 @@ class TopicsController extends Controller
         $topic->update($request->all());
         return $this->response->item($topic, new TopicTransformer());
     }
+
+    public function destroy(Topic $topic)
+    {
+        $this->authorize('destroy', $topic);
+
+        $topic->delete();
+        return $this->response->noContent();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -63,6 +63,7 @@ $api->version('v1', [
             // 发布话题
             $api->post('topics', 'TopicsController@store')->name('api.topics.store');
             $api->patch('topics/{topic}', 'TopicsController@update')->name('api.topics.update');
+            $api->delete('topics/{topic}', 'TopicsController@destroy')->name('api.topics.destroy');
         });
     });
 });
