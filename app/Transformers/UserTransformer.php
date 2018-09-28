149492e6485de6192deab1d03d522370f2b895e7
llilay <2316672811@qq.com>
2018-09-28 07:45:55 +0000

    include user roles

--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -4,9 +4,12 @@ namespace App\Transformers;
 
 use App\Models\User;
 use League\Fractal\TransformerAbstract;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class UserTransformer extends TransformerAbstract
 {
+    protected $availableIncludes = ['roles'];
+
     public function transform(User $user)
     {
         return [
@@ -21,5 +24,10 @@ class UserTransformer extends TransformerAbstract
             'created_at' => $user->created_at->toDateTimeString(),
             'updated_at' => $user->updated_at->toDateTimeString(),
         ];
+    }
+
+    public function includeRoles(User $user)
+    {
+        return $this->collection($user->roles, new RoleTransformer());
     }
 }
