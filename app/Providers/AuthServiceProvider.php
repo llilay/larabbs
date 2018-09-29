9e040856892c594356f68d04ac4132e97f18f5bc
llilay <2316672811@qq.com>
2018-09-29 05:44:59 +0000

    add passport

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
+use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -27,6 +29,13 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+
+        // Passport 的路由
+        Passport::routes();
+        // access_token 过期时间
+        Passport::tokensExpireIn(Carbon::now()->addDays(15));
+        // refreshToken 过期时间
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
 
         \Horizon::auth(function ($request) {
             // 是否是站长
